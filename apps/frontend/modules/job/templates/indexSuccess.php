631724bd4807c8b787fda771d64d6164b1648d4e
Jeremias Fossati <jeremias@Jeremias>
2014-09-04 17:52:05 -0300

    first commit

---
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -0,0 +1,18 @@
+<?php use_stylesheet('jobs.css') ?>
+ 
+<div id="jobs">
+  <?php foreach ($categories as $category): ?>
+    <div class="category_<?php echo Jobeet::slugify($category->getName()) ?>">
+      <div class="category">
+        <div class="feed">
+          <a href="">Feed</a>
+        </div>
+        <h1>
+          <?php echo link_to($category, 'category', $category) ?>
+        </h1>
+      </div>
+ 
+      <?php include_partial('job/list', array('jobs' => $category->getActiveJobs(sfConfig::get('app_max_jobs_on_homepage')))) ?>
+    </div>
+  <?php endforeach; ?>
+</div>
